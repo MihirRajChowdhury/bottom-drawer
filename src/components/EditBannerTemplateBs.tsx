6c9e3b79fda90577873435601aa71435a7b06eff
Mihir Raj <rajmihir945@gmail.com>
2024-08-02 14:10:01 +0530

    Change image functionality added

--- src/components/EditBannerTemplateBs.tsx
+++ src/components/EditBannerTemplateBs.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Box, Modal, TextField, Button } from '@mui/material';
 import { AdBanner } from '../utils/types';
-{/* <TextField label="Image" name="image" value={updatedAd.image} onChange={handleChange} fullWidth margin="normal" /> */}
+
 interface EditBannerTemplateBsProps {
   open: boolean;
   ad: AdBanner;
@@ -21,6 +21,14 @@ const EditBannerTemplateBs: React.FC<EditBannerTemplateBsProps> = ({ open, ad, o
     setUpdatedAd({ ...updatedAd, [name]: value });
   };
 
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      const file = e.target.files[0];
+      const imageUrl = URL.createObjectURL(file);
+      setUpdatedAd({ ...updatedAd, image: imageUrl });
+    }
+  };
+
   const handleSave = () => {
     onSave(updatedAd);
     onClose();
@@ -29,12 +37,56 @@ const EditBannerTemplateBs: React.FC<EditBannerTemplateBsProps> = ({ open, ad, o
   return (
     <Modal open={open} onClose={onClose}>
       <Box sx={{ position: 'absolute', bottom: 0, width: '100%', padding: 2, background: 'white' }}>
-        <TextField label="Title" name="title" value={updatedAd.title} onChange={handleChange} fullWidth margin="normal" />
-        <TextField label="Description" name="description" value={updatedAd.description} onChange={handleChange} fullWidth margin="normal" />
-        <TextField label="CTA" name="cta" value={updatedAd.cta} onChange={handleChange} fullWidth margin="normal" />
+        <TextField
+          label="Title"
+          name="title"
+          value={updatedAd.title}
+          onChange={handleChange}
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          label="Description"
+          name="description"
+          value={updatedAd.description}
+          onChange={handleChange}
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          label="CTA"
+          name="cta"
+          value={updatedAd.cta}
+          onChange={handleChange}
+          fullWidth
+          margin="normal"
+        />
         
-        <TextField label="Background" name="background" value={updatedAd.background} onChange={handleChange} fullWidth margin="normal" />
-        <Button variant="contained" color="primary" onClick={handleSave}>Save</Button>
+        <Button
+          variant="contained"
+          component="label"
+          
+          sx={{ marginY: 2 }}
+        >
+          Change Image
+          <input
+            type="file"
+            accept="image/*"
+            hidden
+            onChange={handleImageChange}
+          />
+        </Button>
+        <TextField
+          label="Background"
+          name="background"
+          value={updatedAd.background}
+          onChange={handleChange}
+          fullWidth
+          margin="normal"
+        />
+        <Button variant="contained" color="primary" onClick={handleSave}>
+          Save
+        </Button>
       </Box>
     </Modal>
   );
